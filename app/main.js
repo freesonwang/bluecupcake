dfe8ad998a6d571a76f937174bc314f918839f1b
Freeson Wang <freeson.wang@yahoo.com>
2017-05-07 20:10:16 +0000

    Drafted out some baby code for world and players. Added lodash and JSONPath

--- app/main.js
+++ app/main.js
@@ -1,14 +1,68 @@
-import Vue from "vue"
+import Vue from "vue";
+import random from "lodash/random";
+import jp from "jsonpath"
+
+class World {
+  constructor () {
+    this.players = [];
+  }
+  
+  static world (spec) {
+    let world = new World;
+    world.players = Player.players(spec);
+    return world;
+  }
+}
+
+class Player {
+  constructor () {
+    this.name = "";
+    this.points = random(10);
+  }
+  
+  static players(spec) {
+    let players = [];
+    let players_spec = jp.query(spec, '$..players')[0];
+    for (let player_spec of players_spec) {
+      let player = new Player();
+      player.name = player_spec.name;
+      player.points = player_spec.points;
+      players.push(player);
+    }
+    return players;
+  }
+}
 
 function main() {
-  var app = new Vue({
+  
+  const CONFIG = {
+    "players": [
+      {
+        "name": "Hillary Clinton",
+        "points": 10
+      },
+      {
+        "name": "Donald Trump",
+        "points": 6
+      }
+    ]
+  }
+  
+  let world = World.world(CONFIG);
+
+  console.log(world);
+  console.log(world.players);
+  document.world = world; // For debugging
+  
+  let app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      world: world
     },
     render: function render(h) {
       return (
-        <h3 style="color:blue">{ this.message }</h3>
+        <h3>{ this.world.players[0].name} - {this.world.players[0].points }</h3>
       )
     }
   });
